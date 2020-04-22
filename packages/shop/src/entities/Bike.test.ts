4ff4b0f4e84ab008c1cd1f0c2959b36501665c54
Gregor <git@grgr.dev>
2020-04-22 11:58:47 +0200

    increase test coverage for bike entity

--- packages/shop/src/entities/Bike.test.ts
+++ packages/shop/src/entities/Bike.test.ts
@@ -3,6 +3,7 @@ import { Bike } from "./Bike"
 describe("Bike", () => {
    it("can be created with all its properties", () => {
       const bike = new Bike(123, "name", 1337, "file.jpg", "description")
+      expect(bike.ean).toBe(123)
       expect(bike.name).toBe("name")
       expect(bike.price).toBe(1337)
       expect(bike.productImageFileName).toBe("file.jpg")
