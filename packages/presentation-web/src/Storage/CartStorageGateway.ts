75089847b7f88e055b741b7c6b06b4ad2b1b5035
Gregor <git@grgr.dev>
2020-04-22 11:48:06 +0200

    use outsourced Cart entity to in CartStorageGateway

--- packages/presentation-web/src/Storage/CartStorageGateway.ts
+++ packages/presentation-web/src/Storage/CartStorageGateway.ts
@@ -1,14 +1,18 @@
 import { StoresCart } from "../Shop/boundaries/StoresCart"
-import { Bike } from "../Shop/entities/Bike"
+import {Bike, Cart} from "@bikeshop/shop";
 
 export class CartStorageGateway implements StoresCart {
-   private _cart: Array<Bike> = []
+   private _cart: Cart
+
+   constructor() {
+      this._cart = new Cart();
+   }
 
    addBike(bike: Bike): void {
-      this._cart.push(bike)
+      this._cart.addBike(bike)
    }
 
    getBikes(): ReadonlyArray<Bike> {
-      return Object.freeze([...this._cart])
+      return Object.freeze([...this._cart.bikes])
    }
 }
