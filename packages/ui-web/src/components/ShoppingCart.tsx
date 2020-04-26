4642a3de3ab873fa57d4cd58dd84d2b13c074522
Gregor <git@grgr.dev>
2020-04-26 18:32:30 +0200

    add remove button to shopping cart

--- packages/ui-web/src/components/ShoppingCart.tsx
+++ packages/ui-web/src/components/ShoppingCart.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from "react"
+import React, { useContext, useState } from "react"
 import { ShopContext } from "../ShopContext"
+import { InlineProgressIndicator } from "./progress-indicators/InlineProgressIndicator"
 
 export function ShoppingCart() {
    const shopContext = useContext(ShopContext)
@@ -8,36 +9,74 @@ export function ShoppingCart() {
 
    if (!shoppingCartViewModel) return null
 
-   const cartBikes = shoppingCartViewModel.bikes.map((bike) => (
-      <div key={bike.name} className="mb-2">
-         <b className="mr-3">{bike.name}</b>
-         <span className="float-right">{bike.price}</span>
-      </div>
+   const cartBikesTableRows = shoppingCartViewModel.bikes.map((bike) => (
+      <tr>
+         <td className="border px-4 py-2">{bike.name}</td>
+         <td className="border px-4 py-2">{bike.price}</td>
+         <td className="border px-4 py-2">
+            <RemoveBikeFromCartButton ean={bike.ean} />
+         </td>
+      </tr>
    ))
 
    return (
       <div className="shadow-lg rounded-lg bg-white mx-auto flex p-8 ">
-         <div className="p-4">
-            <svg
-               xmlns="http://www.w3.org/2000/svg"
-               height="24"
-               viewBox="0 0 24 24"
-               width="24"
-            >
-               <path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" />
-               <path d="M0 0h24v24H0z" fill="none" />
-            </svg>
-         </div>
          <div>
             <h3 className="text-lg text-blue-700">Shopping cart</h3>
             <hr className="mt-2 p-4" />
             <div className="text-sm text-black">
-               {cartBikes}
-               <hr className="p-2" />
-               <span>Total:</span>
-               <b className="float-right">{shoppingCartViewModel.totalPrice}</b>
+               <table className="table-auto">
+                  <tbody>
+                     {cartBikesTableRows}
+                     <tr>
+                        <td className="border px-4 py-2">Total:</td>
+                        <td className="border px-4 py-2">
+                           <b>{shoppingCartViewModel.totalPrice}</b>
+                        </td>
+                        <td className="border px-4 py-2" />
+                     </tr>
+                  </tbody>
+               </table>
             </div>
          </div>
       </div>
+   )
+}
+
+function RemoveBikeFromCartButton(props: { ean: number }) {
+   const [isLoading, setLoading] = useState(false)
+   const shopContext = useContext(ShopContext)
+
+   const handleRemove = async () => {
+      setLoading(true)
+      await shopContext.useCases["RemoveBikeFromCart"].execute({
+         ean: props.ean,
+      })
+      setLoading(false)
+   }
+
+   return (
+      <button
+         className="px-3 py-1 bg-gray-200 text-sm text-gray-900 font-semibold rounded"
+         onClick={handleRemove}
+      >
+         <RemoveFromCartSvgIcon color="steelblue" />
+         {isLoading && <InlineProgressIndicator />}
+      </button>
+   )
+}
+
+function RemoveFromCartSvgIcon(props: { color: string }) {
+   return (
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 24 24"
+         fill={props.color ?? "black"}
+         width="18px"
+         height="18px"
+      >
+         <path d="M22.73 22.73L2.77 2.77 2 2l-.73-.73L0 2.54l4.39 4.39 2.21 4.66-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h7.46l1.38 1.38c-.5.36-.83.95-.83 1.62 0 1.1.89 2 1.99 2 .67 0 1.26-.33 1.62-.84L21.46 24l1.27-1.27zM7.42 15c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h2.36l2 2H7.42zm8.13-2c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H6.54l9.01 9zM7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2z" />
+         <path d="M0 0h24v24H0z" fill="none" />
+      </svg>
    )
 }
