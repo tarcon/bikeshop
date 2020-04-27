c05ae8a39dfa71b55f2f6e319af5e779dd6c0131
Gregor <git@grgr.dev>
2020-04-27 16:11:52 +0200

    add test for bug

--- packages/core-e2e/tests/use-cases/RemoveBikeFromCart.integration.test.ts
+++ packages/core-e2e/tests/use-cases/RemoveBikeFromCart.integration.test.ts
@@ -23,13 +23,13 @@ describe("RemoveBikeFromCart", () => {
       expect(ui.displayCart).toHaveBeenCalledWith({
          bikes: [
             {
-               count: 1,
+               count: 2,
                ean: 456,
-               name: "Second",
-               price: 999,
+               name: "SomeOtherBrand",
+               price: 300,
             },
          ],
-         totalPrice: 999,
+         totalPrice: 600,
       })
    })
 
@@ -41,16 +41,19 @@ describe("RemoveBikeFromCart", () => {
          displayCart: jest.fn(),
       }
 
-      const cartWithTwoBikes = new Cart()
-      cartWithTwoBikes.addProduct(
-         aBike({ name: "First", ean: 123, price: 1337 })
+      const cartWithThreeBikes = new Cart()
+      cartWithThreeBikes.addProduct(
+         aBike({ name: "SomeBrand", ean: 123, price: 100 })
       )
-      cartWithTwoBikes.addProduct(
-         aBike({ name: "Second", ean: 456, price: 999 })
+      cartWithThreeBikes.addProduct(
+         aBike({ name: "SomeOtherBrand", ean: 456, price: 300 })
+      )
+      cartWithThreeBikes.addProduct(
+         aBike({ name: "SomeOtherBrand", ean: 456, price: 300 })
       )
 
       cartStorageGateway = new CartStorageGateway()
-      cartStorageGateway.store(cartWithTwoBikes)
+      cartStorageGateway.store(cartWithThreeBikes)
 
       useCase = new RemoveBikeFromCart(ui, cartStorageGateway)
    })
