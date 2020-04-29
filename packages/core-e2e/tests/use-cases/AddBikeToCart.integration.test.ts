8c81fdc0e497fa0560fc82a6f5bbef96882e0ce4
Gregor <git@grgr.dev>
2020-04-29 17:58:41 +0200

    test presenter in e2e test as well

--- packages/core-e2e/tests/use-cases/AddBikeToCart.integration.test.ts
+++ packages/core-e2e/tests/use-cases/AddBikeToCart.integration.test.ts
@@ -1,13 +1,23 @@
-import { AddBikeToCart, DisplaysCart, DisplaysError } from "@bikeshop/core"
+import {
+   AddBikeToCart,
+   CartPresenter,
+   CartViewModel,
+   DisplaysCart,
+   DisplaysError,
+} from "@bikeshop/core"
 import { BikeBackendGateway } from "@bikeshop/network"
 import { CartStorageGateway } from "@bikeshop/storage"
 
 describe("AddBikeToCart", () => {
    let ui: DisplaysError & DisplaysCart
 
-   it("displays the shopping cart with two bike", async () => {
+   it("displays the shopping cart with two bike", async (done) => {
       const backend = new BikeBackendGateway()
       const cart = new CartStorageGateway()
+      const uiCalls: Array<CartViewModel> = []
+      const ui = new CartPresenter((viewModel: CartViewModel) => {
+         uiCalls.push(viewModel)
+      })
 
       const useCase = new AddBikeToCart(backend, cart, ui)
 
@@ -18,18 +28,31 @@ describe("AddBikeToCart", () => {
       await useCase.execute(bikeToAdd)
       await useCase.execute(bikeToAdd)
 
-      expect(ui.displayError).not.toHaveBeenCalled()
-      expect(ui.displayCart).toHaveBeenCalledWith({
+      expect(uiCalls[0]).toStrictEqual({
+         bikes: [
+            {
+               count: 1,
+               ean: 123908123,
+               name: "Carbono R3",
+               price: "4.499,00 €",
+            },
+         ],
+         totalPrice: "4.499,00 €",
+      })
+
+      expect(uiCalls[1]).toStrictEqual({
          bikes: [
             {
                count: 2,
                ean: 123908123,
                name: "Carbono R3",
-               price: 4499,
+               price: "4.499,00 €",
             },
          ],
-         totalPrice: 8998,
+         totalPrice: "8.998,00 €",
       })
+
+      done()
    })
 
    it("stores the cart after adding a bike", async () => {
