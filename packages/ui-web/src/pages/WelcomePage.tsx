f3829c2c03c8fb10a98e7ed153c7a9ca56de2cb7
Gregor Schulz <gregor.schulz@qualitypool.de>
2021-09-06 11:02:20 +0200

    improve styling

--- packages/ui-web/src/pages/WelcomePage.tsx
+++ packages/ui-web/src/pages/WelcomePage.tsx
@@ -3,18 +3,17 @@ import { WelcomeViewModel } from "@bikeshop/core"
 
 export function WelcomePage(props: { welcomeViewModel: WelcomeViewModel }) {
    return (
-      <div>
-         <div>
+      <div className="row">
+         <div className="col-6-md">
             <p>{props.welcomeViewModel.welcomeText}</p>
          </div>
-         <div>
-            <div>
-               <img
-                  src="./img/cleancode.jpg"
-                  width="200"
-                  alt="Powered by clean code"
-               />
-            </div>
+
+         <div className="col-3-md">
+            <img
+               src="./img/cleancode.jpg"
+               width="100"
+               alt="Powered by clean code"
+            />
          </div>
       </div>
    )
