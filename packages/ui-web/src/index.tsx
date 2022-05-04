8ad883ea26a2efc3c1c6cc0397812e847936ca77
tarcon <gregor.schulz@qualitypool.de>
2022-05-04 10:16:03 +0200

    fix warning

--- packages/ui-web/src/index.tsx
+++ packages/ui-web/src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 import App from "./App"
 import { ShopContextProvider } from "./ShopContextProvider"
 
-ReactDOM.render(
-   <>
-      <ShopContextProvider>
-         <App />
-      </ShopContextProvider>
-   </>,
-   document.getElementById("root")
-)
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container!);
+
+root.render(
+   <ShopContextProvider>
+      <App />
+   </ShopContextProvider>
+);
