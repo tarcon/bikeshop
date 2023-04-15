d31d95d6ca9ed26be6b08eb37710c03984c792a4
Gregor Schulz <gregor.schulz@qualitypool.de>
2023-04-15 12:36:39 +0200

    upgrade deps

--- jest.config.base.js
+++ jest.config.base.js
@@ -2,11 +2,11 @@ module.exports = {
    roots: ["<rootDir>/"],
    testMatch: ["**/*.test.ts"],
    transform: {
-      "^.+\\.tsx?$": "ts-jest",
-   },
-   globals: {
-      "ts-jest": {
-         tsconfig: "../../tsconfig.test.json",
-      },
+      "^.+\\.tsx?$": [
+         "ts-jest",
+         {
+            tsconfig: "../../tsconfig.test.json",
+         },
+      ],
    },
 }
